e56053b3c68efb1a598c8fa7a3998222420c7368
Deivid012-12 <derodriguezp@unbosque.edu.co>
2026-02-09 23:06:36 -0500

    prueba video

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://Deivid012-12.github.io',
-    base: '/',
+    base: '/Deivid012-12.github.io',
     output: 'static'
 });
